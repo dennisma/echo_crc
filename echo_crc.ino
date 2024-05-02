72cfdb82ec0fb69322901ff45d8e9bcee3a48c45
dennisma <denbo68@gmail.com>
2024-05-02 13:18:09 -0400

    Update echo_crc.ino
    done

--- echo_crc.ino
+++ echo_crc.ino
@@ -1,4 +1,6 @@
 #include "piComm.h"
+#include <string>
+#include <sstream>
 
 piComm pi(Serial1);
 
@@ -7,18 +9,52 @@ void setup() {
   Serial.begin(152000);  //init Serial
 }
 
-int count = 2;
+int count = 0;
+
+int lspeed, rspeed;
+
+void parseMe(std::string& data) {
+  using namespace std;
+  char ch;
+  stringstream ss(data);
+
+  if (ss.peek() == '^')
+  {
+    ss >> ch;
+
+    if (ss.peek() == 'L')
+    {
+      ss >> ch;
+      ss >> lspeed;
+      Serial.print(lspeed);
+    }
+    if (ss.peek() == 'R')
+    {
+      ss >> ch;
+      ss >> rspeed;
+      
+      Serial.println(rspeed);
+    }
+  }
+  else if (ss.peek() == '?')
+  {
+    stringstream newss;
+    newss << "^L" << lspeed << 'R' << rspeed;
+    pi.send(newss.str());
+  }
+}
+
 
 void loop() {
+  //Serial.println(pi.msgWaiting());
+  //delay(1000);
   if (pi.msgWaiting()) {
     auto [success, data] = pi.read();
 
     if (success) {
-      if (!count--) {
-        Serial.printf("(%d,%s)\n", success, data.c_str());
-        count = 100;
-      }
-      pi.send(data);
+      Serial.printf("(%d,%s)\n", success, data.c_str());
+      parseMe(data);
+      //pi.send(data);
     } else {
       Serial.printf("(%d,None)\n", success);
       while (1)
@@ -28,5 +64,5 @@ void loop() {
 
 
 
-  
+
 }
